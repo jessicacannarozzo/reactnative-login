794e96a1f8dcd770b1d421153d17c50ab001fb4f
Jess <jesscannarozzo@cmail.carleton.ca>
2018-01-03 13:45:19 -0500

    random rgb working, but not in a timely manner

--- App.js
+++ App.js
@@ -16,8 +16,24 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: spiceBackgroundColor(),
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+//Firebase:
+
+
+//Other Functions
+
+//assigns background a random color
+//return: rgb string. r,g,b are all between 0 and 255
+function spiceBackgroundColor() {
+  return "rgb("+randomInt(0,255)+","+randomInt(0,255)+","+randomInt(0,255)+")";
+}
+
+//returns random value between min and max
+function randomInt(min,max) {
+  return Math.random() * (max - min) + min;
+}
